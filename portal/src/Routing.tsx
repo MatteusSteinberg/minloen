cf3f7f7ddad58b6b6ee36a6710155d8b8565fc40
Jonas Bang <jonasbang@hotmail.dk>
2024-05-21 12:57:40 +0200

    init

---
+++ portal/src/Routing.tsx
@@ -0,0 +1,50 @@
+import { lazy } from "react";
+
+// Components
+import { Route, Routes, matchPath, useLocation } from "react-router-dom";
+import Sidebar from "./components/globals/Sidebar";
+
+// Pages
+const Dashboard = lazy(() => import("./routes/dashboard"));
+const Login = lazy(() => import("./routes/login"));
+const Signup = lazy(() => import("./routes/signup"));
+
+interface IAppRoute {
+  path: string;
+  element: React.ReactNode;
+  protected?: boolean;
+  layout?: boolean;
+  exact?: boolean;
+}
+
+const routes: Array<IAppRoute> = [
+  { path: "/", element: <Dashboard />, layout: true },
+  { path: "/signup", element: <Signup />, layout: false },
+  { path: "/login", element: <Login />, layout: false },
+];
+
+const Routing = () => {
+  const { pathname } = useLocation();
+
+  const route = routes.find((x) =>
+    !x.exact ? matchPath(x.path, pathname) : x.path === pathname
+  );
+  const showLayout = route?.layout;
+
+  return (
+    <main className="relative flex items-start justify-between w-full p-6 min-h-dvh">
+      {showLayout && (
+        <aside>
+          <Sidebar />
+        </aside>
+      )}
+      <Routes>
+        {routes.map((route) => (
+          <Route key={route.path} path={route.path} element={route.element} />
+        ))}
+      </Routes>
+    </main>
+  );
+};
+
+export default Routing;
---
+++ portal/src/routes/dashboard.tsx
@@ -0,0 +1,7 @@
+type Props = {};
+
+const dashboard = (props: Props) => {
+  return <h1>dashby</h1>;
+};
+
+export default dashboard;
