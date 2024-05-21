ca9c1772f2c076ac34a28665d3d635ff7c3adeed
Jonas Bang <jonasbang@hotmail.dk>
2024-05-21 10:00:10 +0200

    Added tailwindcss config

---
+++ portal/tailwind.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      xs: "420px",
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+      xxl: "1400px",
+    },
+    colors: {
+      primary: "#55FEC3",
+      primaryLight: "#CDE4BA",
+      primarySupport: "#141718",
+      secondarySupport: "#232627",
+      tertiarySupport: "#8FB996",
+      text: "#252525",
+      border: "#E7E7E9",
+      error: "#C35865",
+      success: "#58C383",
+      white: "#FFFFFF",
+      black: "#000000",
+      gradient:
+        "linear-gradient(90deg, rgba(36, 40, 51, 0.50) 0%, rgba(50, 51, 55, 0.50) 53.86%)",
+    },
+    fontFamily: {
+      default: ["Mundial", "sans-serif"],
+    },
+    easing: {
+      default: "cubic-bezier(0.65, 0, 0.35, 1)",
+    },
+    shadows: {
+      default: "0px 2px 75px 0px rgba(37, 37, 37, 0.10);",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
