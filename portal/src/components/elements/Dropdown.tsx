1972ede7399d126d431b0dad4d380417a316e864
Emil <emilskovmand@gmail.com>
2024-05-27 12:02:02 +0200

    new coworker fields

--- portal/src/components/elements/Dropdown.tsx
+++ portal/src/components/elements/Dropdown.tsx
@@ -1,17 +1,20 @@
 import { useEffect, useMemo, useRef, useState } from "react"
 import { useOutsideClick } from "../../hooks/useOutsideClick"
 
+export type DropdownOption = { value: any, label: string }
+
 type Props = {
-  options: { value: any, label: string }[]
+  options: Array<DropdownOption>
   spacing?: boolean
   error?: string
   value?: any
+  placeholder?: string
   onChange?: (v: any) => void
   name: string
   label?: string
 }
 
-const Dropdown = ({ options, name, error, label, onChange, spacing, value }: Props) => {
+const Dropdown = ({ options, name, placeholder, error, label, onChange, spacing, value }: Props) => {
   const [selectedOption, setSelectedOption] = useState<any>(value)
   const [dropdownDirection, setDropdownDirection] = useState("mt-1")
   const buttonRef = useRef<HTMLButtonElement>(null)
@@ -51,11 +54,13 @@ const Dropdown = ({ options, name, error, label, onChange, spacing, value }: Pro
     }
   }, [isVisible])
 
+  const isPlaceholder = !currentOption && placeholder
+
   return (
-    <div className={`relative block w-full ${spacing ? "mb-4" : "mb-0"} max-w-96`}>
+    <div className={`relative block w-full ${spacing ? "mb-4" : "mb-0"}`}>
       <label className="block mb-3 text-white">{label}</label>
       <button className={`px-5 relative rounded-[14px] text-white w-full h-[68px] inline-flex items-center justify-start border-[1px] border-solid ${error ? "border-error" : "border-[#33363E]"} bg-[rgba(33,33,34,0.2)]`} ref={buttonRef} onClick={() => toggleVisibility(!isVisible)}>
-        <span>{currentOption?.label || "Select an Option"}</span>
+        <span className={isPlaceholder ? "text-[rgba(255,255,255,0.3)]" : undefined}>{currentOption?.label || placeholder || "Select an Option"}</span>
         <svg className="absolute right-4 top-1/2 -transform-y-1/2" width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M1 1L4.29289 4.29289C4.68342 4.68342 5.31658 4.68342 5.70711 4.29289L9 1" stroke="#767676" stroke-width="2" stroke-linecap="round" />
         </svg>
