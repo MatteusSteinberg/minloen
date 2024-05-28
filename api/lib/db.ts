f8f76e3239e1a33fe1eb2548b7d3c434a3f41da3
MatteusSteinberg <53737794+MatteusSteinberg@users.noreply.github.com>
2024-05-28 15:11:49 +0200

    added perms to action file

--- api/lib/db.ts
+++ api/lib/db.ts
@@ -4,7 +4,7 @@ mongoose.set("debug", process.env.NODE_ENV !== "production")
 
 mongoose.set("autoIndex", true)
 
-mongoose.set("maxTimeMS", 10000) // TE
+mongoose.set("maxTimeMS", 10000) // TEST
 
 let connection: typeof import("mongoose") | undefined
 
