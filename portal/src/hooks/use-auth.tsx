e849e79a1ecc809d6914381a6d509f300687aa8b
Emil <emilskovmand@gmail.com>
2024-06-03 09:25:26 +0200

    nice

--- portal/src/hooks/use-auth.tsx
+++ portal/src/hooks/use-auth.tsx
@@ -6,72 +6,73 @@ import { IUser } from "../../../interfaces/user.interface"
 import { IRequestData, requestWithBody, useAPI } from "./use-api"
 
 interface IAuth {
-    authenticate: (email: string, password: string) => Promise<{ error?: any }>
-    unauthenticate: () => void
-    updateMe: (user: Partial<IUser>) => Promise<IRequestData<IUser>>
-    user?: IUser
+  authenticate: (email: string, password: string) => Promise<{ error?: any }>
+  unauthenticate: () => void
+  updateMe: (user: Partial<IUser>) => Promise<IRequestData<IUser>>
+  user?: IUser
 }
 
 interface IAuthToken {
-    /** ISO string Date */
-    expires: string
-    token: string
+  /** ISO string Date */
+  expires: string
+  token: string
 }
 
 const AuthContext = React.createContext<IAuth>({} as any)
 
 export const useAuth = () => {
-    return useContext<IAuth>(AuthContext)
+  return useContext<IAuth>(AuthContext)
 }
 
 const api = (process.env.REACT_APP_API || "") + "/api"
 
 export const AuthProvider = memo(({ children }: { children: React.ReactNode }) => {
-    const [token, setToken, clearToken] = useLocalStorage<string>("token")
+  const [token, setToken, clearToken] = useLocalStorage<string>("token")
 
-    if (token) {
-        axios.defaults.headers.common.Authorization = `Bearer ${token}`
+  if (token) {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`
+  } else {
+    delete axios.defaults.headers.common.Authorization
+  }
+
+  const { data, update, mutate, setData } = useAPI<IUser>({ url: "/user" })
+
+  const authenticate = async (email: string, password: string) => {
+    const result = await requestWithBody(`${api}/user/login`, "post", {
+      email,
+      password,
+    })
+
+    if (result?.data) {
+      const token = (result.data as IAuthToken).token
+      setToken(token)
+      axios.defaults.headers.common.Authorization = `Bearer ${token}`
+      await mutate()
     } else {
-        delete axios.defaults.headers.common.Authorization
+      return { error: result?.error }
     }
 
-    const { data, update, mutate, setData } = useAPI<IUser>({ url: "/user" })
+    return {}
+  }
 
-    const authenticate = async (email: string, password: string) => {
-        const result = await requestWithBody(`${api}/user/login`, "post", {
-            email,
-            password,
-        })
+  const unauthenticate = async () => {
+    clearToken()
+    setData(undefined)
+    delete axios.defaults.headers.common.Authorization
+  }
 
-        if (result?.data) {
-            const token = (result.data as IAuthToken).token
-            setToken(token)
-            axios.defaults.headers.common.Authorization = `Bearer ${token}`
-            await mutate()
-        } else {
-            return { error: result?.error }
-        }
+  const updateMe = async (user: Partial<IUser>) => {
+    setData(_cloneDeep(user))
+    console.log(user)
+    return await update(user)
+  }
 
-        return {}
-    }
+  const contextValue = {
+    user: data,
+    authenticate,
+    unauthenticate,
+    updateMe,
+  }
 
-    const unauthenticate = async () => {
-        clearToken()
-        setData(undefined)
-        delete axios.defaults.headers.common.Authorization
-    }
-
-    const updateMe = async (user: Partial<IUser>) => {
-        setData(_cloneDeep(user))
-        return await update({ body: user })
-    }
-
-    const contextValue = {
-        user: data,
-        authenticate,
-        unauthenticate,
-        updateMe,
-    }
-
-    return <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
+  return <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
 })
