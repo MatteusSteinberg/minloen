9f15a8043f9990a58e2903c029f82f0ec1214427
MatteusSteinberg <53737794+MatteusSteinberg@users.noreply.github.com>
2024-06-03 23:48:24 +0200

    Build API now

--- api/routes/user.ts
+++ api/routes/user.ts
@@ -1,21 +1,20 @@
-import express from "express";
-import { add, get, list, listMetadata, login, me, update, updateMe } from "../services/user.route";
+import express from "express"
+import { add, get, list, listMetadata, login, me, update, updateMe } from "../services/user.route"
 
 const userRouter = express.Router()
 
-
 // POST
-userRouter.post('/login', login)
-userRouter.post('/', add)
+userRouter.post("/login", login)
+userRouter.post("/", add)
 
 // GET
-userRouter.get('/', me)
-userRouter.get('/list', list)
-userRouter.get('/:id', get)
-userRouter.get('/list/meta', listMetadata)
+userRouter.get("/", me)
+userRouter.get("/list", list)
+userRouter.get("/:id", get)
+userRouter.get("/list/meta", listMetadata)
 
 // PATCH
-userRouter.patch('/', updateMe)
-userRouter.patch('/:id', update)
+userRouter.patch("/", updateMe)
+userRouter.patch("/:id", update)
 
 export default userRouter
