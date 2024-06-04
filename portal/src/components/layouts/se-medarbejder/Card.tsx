24414721ca2d747ea04bed7c45607db1820fcbe5
Jonas Bang <jonasbang@hotmail.dk>
2024-06-05 00:02:20 +0200

    Fixed responsive "Se medarbejder"

--- portal/src/components/layouts/se-medarbejder/Card.tsx
+++ portal/src/components/layouts/se-medarbejder/Card.tsx
@@ -9,10 +9,10 @@ interface ICard {
 
 const Card = ({ description, icon, title, type }: ICard) => {
     return (
-        <div className="px-6 py-6 bg-white border border-solid shadow-custom border-lightBorder dark:border-darkBorder rounded-2xl dark:bg-darkPrimarySupport">
-            <div className="flex items-center justify-between">
-                <div className="flex flex-row items-center gap-4 pr-4">
-                    <div className={`h-10 w-10 flex items-center justify-center rounded-full ${type === "warning" ? "bg-[rgba(253,154,82,0.10)]" : "bg-[#F5F7F3]"}`}>{icon}</div>
+        <div className="p-4 bg-white border border-solid sm:px-6 sm:py-6 shadow-custom border-lightBorder dark:border-darkBorder rounded-2xl dark:bg-darkPrimarySupport">
+            <div className="flex flex-col justify-between gap-4 sm:gap-0 sm:items-center sm:flex-row">
+                <div className="flex flex-row gap-4 sm:items-center sm:pr-4">
+                    <div className={`h-10 w-10 flex-shrink-0 flex items-center justify-center rounded-full ${type === "warning" ? "bg-[rgba(253,154,82,0.10)]" : "bg-[#F5F7F3]"}`}>{icon}</div>
                     <div className="flex flex-col">
                         <p className="text-text dark:text-white font-standard-medium">{title}</p>
                         <p className={`font-small-medium ${type === "warning" ? "text-[#FD9A52]" : "text-text dark:text-white opacity-30"}`}>{description}</p>
--- portal/src/routes/se-medarbejder.tsx
+++ portal/src/routes/se-medarbejder.tsx
@@ -11,142 +11,142 @@ import { useAPI } from "../hooks/use-api"
 import { workplacePensionInstitues } from "./ny-medarbejder"
 
 interface IAgreements {
-  type: string
-  icon: React.ReactNode
-  title: string
-  description: string
+    type: string
+    icon: React.ReactNode
+    title: string
+    description: string
 }
 
 const coworkersData = {
-  headers: ["ID #", "Dato", "Til udbetaling"],
-  rows: [
-    {
-      "ID #": 1,
-      Dato: "15/01/2024",
-      "Til udbetaling": "18.057",
-    },
-    {
-      "ID #": 2,
-      Dato: "15/01/2024",
-      "Til udbetaling": "18.057",
-    },
-    {
-      "ID #": 3,
-      Dato: "15/01/2024",
-      "Til udbetaling": "18.057",
-    },
-    {
-      "ID #": 4,
-      Dato: "15/01/2024",
-      "Til udbetaling": "18.057",
-    },
-    {
-      "ID #": 5,
-      Dato: "15/01/2024",
-      "Til udbetaling": "18.057",
-    },
-    {
-      "ID #": 6,
-      Dato: "15/01/2024",
-      "Til udbetaling": "18.057",
-    },
-  ],
+    headers: ["ID #", "Dato", "Til udbetaling"],
+    rows: [
+        {
+            "ID #": 1,
+            Dato: "15/01/2024",
+            "Til udbetaling": "18.057",
+        },
+        {
+            "ID #": 2,
+            Dato: "15/01/2024",
+            "Til udbetaling": "18.057",
+        },
+        {
+            "ID #": 3,
+            Dato: "15/01/2024",
+            "Til udbetaling": "18.057",
+        },
+        {
+            "ID #": 4,
+            Dato: "15/01/2024",
+            "Til udbetaling": "18.057",
+        },
+        {
+            "ID #": 5,
+            Dato: "15/01/2024",
+            "Til udbetaling": "18.057",
+        },
+        {
+            "ID #": 6,
+            Dato: "15/01/2024",
+            "Til udbetaling": "18.057",
+        },
+    ],
 }
 
 const WarningIcon = <ExclamationTriangleIcon className="w-5 h-5 text-[#FD9A52]" />
 const SuccessIcon = <CheckIcon className="w-5 h-5 text-[#3D8545]" />
 
 const SeeCoworker = () => {
-  const { id } = useParams()
+    const { id } = useParams()
 
-  const { data } = useAPI<IUser>({ url: "/user", id })
+    const { data } = useAPI<IUser>({ url: "/user", id })
 
-  const agreements = useMemo(() => {
-    const agreementDictionaries = {
-      salaryType: {
-        hourly: {
-          title: "Personen er timelønnet",
-          description: "Denne medarbejder er oprettet som timelønnet",
-        },
-        salary: {
-          title: "Personen er fastlønnet",
-          description: "Denne medarbejder er oprettet til at modtage gage",
-        },
-      } as any,
-      workplacePension: {} as any,
-      vacation: {
-        vacationSavings: {
-          title: `${data?.name} har ferie opsparing`,
-          description: "Denne medarbejder er sat til at modtage ferie opsparing",
-        },
-        vacationWithPay: {
-          title: `${data?.name} har ferie med løn`,
-          description: "Denne medarbejder er sat at få modtage ferie med løn",
-        },
-      } as any,
-      paymentArrangement: {
-        ahead: {
-          title: `${data?.name} får løn måneden forud`,
-          description: `Betales måneden forud`
-        },
-        behind: {
-          title: `${data?.name} får løn måneden bagud`,
-          description: `Betales måneden bagud`
+    const agreements = useMemo(() => {
+        const agreementDictionaries = {
+            salaryType: {
+                hourly: {
+                    title: "Personen er timelønnet",
+                    description: "Denne medarbejder er oprettet som timelønnet",
+                },
+                salary: {
+                    title: "Personen er fastlønnet",
+                    description: "Denne medarbejder er oprettet til at modtage gage",
+                },
+            } as any,
+            workplacePension: {} as any,
+            vacation: {
+                vacationSavings: {
+                    title: `${data?.name} har ferie opsparing`,
+                    description: "Denne medarbejder er sat til at modtage ferie opsparing",
+                },
+                vacationWithPay: {
+                    title: `${data?.name} har ferie med løn`,
+                    description: "Denne medarbejder er sat at få modtage ferie med løn",
+                },
+            } as any,
+            paymentArrangement: {
+                ahead: {
+                    title: `${data?.name} får løn måneden forud`,
+                    description: `Betales måneden forud`,
+                },
+                behind: {
+                    title: `${data?.name} får løn måneden bagud`,
+                    description: `Betales måneden bagud`,
+                },
+            } as any,
         }
-      } as any
-    }
 
-    return [
-      {
-        type: data?.salaryType ? "approved" : "warning",
-        icon: data?.salaryType ? SuccessIcon : WarningIcon,
-        title: data?.salaryType ? agreementDictionaries.salaryType[data.salaryType].title : `${data?.name} modtager ikke løn`,
-        description: data?.salaryType ? agreementDictionaries.salaryType[data.salaryType].description : `Denne medarbejder ikke sat til at modtage løn`,
-      },
-      {
-        type: !!data?.vacation?.scheme ? "approved" : "warning",
-        icon: !!data?.vacation?.scheme ? SuccessIcon : WarningIcon,
-        title: !!data?.vacation?.scheme ? agreementDictionaries.vacation[data?.vacation?.scheme].title : "Personen får ikke betalt ferie",
-        description: !!data?.vacation?.scheme ? agreementDictionaries.vacation[data?.vacation?.scheme].description : "Denne medarbejder er ikke sat til at modtage betalt ferie",
-      },
-      {
-        type: !!data?.workplacePension?.institute ? "approved" : "warning",
-        icon: !!data?.workplacePension?.institute ? SuccessIcon : WarningIcon,
-        title: !!data?.workplacePension?.institute ? "Personen får pension" : "Personen får ikke pension",
-        description: !!data?.workplacePension?.institute ? `Denne medarbejder modtager pension fra ${workplacePensionInstitues.find((x) => x.value === data?.workplacePension?.institute)?.label ?? `Ukendt ${data?.workplacePension?.institute}`}` : "Denne medarbejder modtager ikke pension",
-      },
-      {
-        type: !!data?.paymentArrangement ? "approved" : "warning",
-        icon: !!data?.paymentArrangement ? SuccessIcon : WarningIcon,
-        title: !!data?.paymentArrangement ? agreementDictionaries.paymentArrangement[data?.paymentArrangement].title : "Personen mangler en betalingsordning",
-        description: !!data?.paymentArrangement ? agreementDictionaries.paymentArrangement[data?.paymentArrangement].description : "Denne medarbejder har ikke fået opsat en betalingsordning til sin løn"
-      }
-    ]
-  }, [data])
+        return [
+            {
+                type: data?.salaryType ? "approved" : "warning",
+                icon: data?.salaryType ? SuccessIcon : WarningIcon,
+                title: data?.salaryType ? agreementDictionaries.salaryType[data.salaryType].title : `${data?.name} modtager ikke løn`,
+                description: data?.salaryType ? agreementDictionaries.salaryType[data.salaryType].description : `Denne medarbejder ikke sat til at modtage løn`,
+            },
+            {
+                type: !!data?.vacation?.scheme ? "approved" : "warning",
+                icon: !!data?.vacation?.scheme ? SuccessIcon : WarningIcon,
+                title: !!data?.vacation?.scheme ? agreementDictionaries.vacation[data?.vacation?.scheme].title : "Personen får ikke betalt ferie",
+                description: !!data?.vacation?.scheme ? agreementDictionaries.vacation[data?.vacation?.scheme].description : "Denne medarbejder er ikke sat til at modtage betalt ferie",
+            },
+            {
+                type: !!data?.workplacePension?.institute ? "approved" : "warning",
+                icon: !!data?.workplacePension?.institute ? SuccessIcon : WarningIcon,
+                title: !!data?.workplacePension?.institute ? "Personen får pension" : "Personen får ikke pension",
+                description: !!data?.workplacePension?.institute ? `Denne medarbejder modtager pension fra ${workplacePensionInstitues.find((x) => x.value === data?.workplacePension?.institute)?.label ?? `Ukendt ${data?.workplacePension?.institute}`}` : "Denne medarbejder modtager ikke pension",
+            },
+            {
+                type: !!data?.paymentArrangement ? "approved" : "warning",
+                icon: !!data?.paymentArrangement ? SuccessIcon : WarningIcon,
+                title: !!data?.paymentArrangement ? agreementDictionaries.paymentArrangement[data?.paymentArrangement].title : "Personen mangler en betalingsordning",
+                description: !!data?.paymentArrangement ? agreementDictionaries.paymentArrangement[data?.paymentArrangement].description : "Denne medarbejder har ikke fået opsat en betalingsordning til sin løn",
+            },
+        ]
+    }, [data])
 
-  return (
-    <ContentContainer>
-      <div>
-        <Header title={data?.name ?? ""} history="/medarbejdere" />
-      </div>
-      <Options canCreatePayroll={!!data?.salaryType} />
-      <div className="flex items-start justify-between">
-        <div className="flex flex-col items-start w-full">
-          <p className="text-text dark:text-white font-large-semibold">Aftaler</p>
-          <p className="text-text dark:text-white opacity-30">Tilpassede medarbejder aftaler</p>
-        </div>
-        <div className="flex flex-col w-full gap-3">
-          {agreements.map((agreement: IAgreements, index: number) => (
-            <Card key={index} {...agreement} />
-          ))}
-        </div>
-      </div>
-      <div className="flex flex-col items-start justify-between gap-4 mt-16">
-        <DataTable title="Lønsedler" tableData={coworkersData} />
-        <DataTable title="Kørsel" tableData={coworkersData} />
-      </div>
-    </ContentContainer>
-  )
+    return (
+        <ContentContainer>
+            <div>
+                <Header title={data?.name ?? ""} history="/medarbejdere" />
+            </div>
+            <Options canCreatePayroll={!!data?.salaryType} />
+            <div className="flex flex-col items-start justify-between gap-4 xxl:gap-0 xxl:flex-row">
+                <div className="flex flex-col items-start w-full">
+                    <p className="text-text dark:text-white font-large-semibold">Aftaler</p>
+                    <p className="text-text dark:text-white opacity-30">Tilpassede medarbejder aftaler</p>
+                </div>
+                <div className="flex flex-col w-full gap-3">
+                    {agreements.map((agreement: IAgreements, index: number) => (
+                        <Card key={index} {...agreement} />
+                    ))}
+                </div>
+            </div>
+            <div className="flex flex-col items-start justify-between gap-4 mt-16">
+                <DataTable title="Lønsedler" tableData={coworkersData} />
+                <DataTable title="Kørsel" tableData={coworkersData} />
+            </div>
+        </ContentContainer>
+    )
 }
 
 export default SeeCoworker
