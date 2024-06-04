4cd533429c2f9c495b8f01129549d408edf1c27d
Christian Kirk Ludvigsen <christianludvigsendk@gmail.com>
2024-06-04 11:37:14 +0200

    Update DrivingModal.tsx

--- portal/src/components/modals/DrivingModal.tsx
+++ portal/src/components/modals/DrivingModal.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import DatePicker from 'react-date-picker';
+import Checkbox from "../elements/Checkbox";
+import Input from "../elements/Input";
 import Base from "./Base";
 import './customDatePicker.css';
 
@@ -9,6 +11,11 @@ interface IDrivingModal {
 }
 
 const DrivingModal = ({ isOpen, toggleModal }: IDrivingModal) => {
+
+    const retur = {
+        km: 0,
+    }
+    console.log(retur)
 
     const [value, setValue] = useState<[Date, Date] | undefined>([new Date(), new Date()]);
 
@@ -28,6 +35,9 @@ const DrivingModal = ({ isOpen, toggleModal }: IDrivingModal) => {
                     <input className="rounded-[8px] h-[48.6px] border-[1px] border-solid text-white border-darkBorder outline-0 bg-[rgba(33,33,34,0.2)] py-4 pl-4" name="Fra" placeholder="fks. Aarhus"></input>
                     <input className="rounded-[8px] h-[48.6px] border-[1px] border-solid text-white border-darkBorder outline-0 bg-[rgba(33,33,34,0.2)] py-4 pl-4 justify-center align items-center absolute right-12" name="Til" placeholder="fks. København"></input>
                 </div>
+                <div>
+                    <Checkbox name="retur" text="Tur-retur" value=""></Checkbox>
+                </div>
                 <div className="my-6">
                     <h1 className="block mb-3 text-white">
                         Dato KM
@@ -36,6 +46,9 @@ const DrivingModal = ({ isOpen, toggleModal }: IDrivingModal) => {
                         <DatePicker className="custom-date-range-picker" onChange={(value) => setValue(value as [Date, Date] | undefined)} value={value} />
                         <input className="rounded-[8px] h-[48.6px] border-[1px] border-solid text-white border-darkBorder outline-0 bg-[rgba(33,33,34,0.2)] py-4 pl-4 justify-center align items-center absolute right-12" type="number" name="km" placeholder="antal KM"></input>
                     </div>
+                </div>
+                <div>
+                    <Input name="Nummerplade" placeholder="AB12345" label="Nummerplade"></Input>
                 </div>
                 <div className="my-12">
                     <button className="w-[120px] h-[44px] bg-darkPrimaryLight text-text rounded-xl border border-solid border-darkBorder mr-3">Tilføj</button>
